34ccfa4fc9e8c1e91725472d7462ffab6a15a264
Eric Wright <eric.wright@turbonomic.com>
2020-03-29 11:23:00 -0400

    updating some syntax to fix variable issues

--- artifacts/terraform/main.tf
+++ artifacts/terraform/main.tf
@@ -41,9 +41,9 @@ resource "aws_security_group" "instance" {
 resource "aws_instance" "cert33" {
   ami = "ami-08aaa44ddb42288f1"
   instance_type = "t2.large"
-  key_name = "var.aws_key_name"
-  subnet_id = "var.aws_launch_subnet"
-  vpc_security_group_ids = [ "aws_security_group.instance.id" ]
+  key_name = "${var.aws_key_name}"
+  subnet_id = "${var.aws_launch_subnet}"
+  vpc_security_group_ids = [ "${saws_security_group.instance.id}" ]
   tags = {
         Name = "cert33"
         ProvisionedBy = "Project Terra"
@@ -56,7 +56,7 @@ resource "aws_alb_target_group" "instance" {
     name = "cert33-target-group"
     port = 443
     protocol = "HTTPS"
-    vpc_id = "var.aws_vpc_id"
+    vpc_id = "${var.aws_vpc_id}"
 }
  
 ### Create ALB
@@ -65,7 +65,7 @@ resource "aws_alb" "instance" {
     name = "cert33-elb"
     internal = false
     subnets = ["subnet-07d8310503d3d9704","subnet-091d4411443e82465"]
-    security_groups = [ "aws_security_group.instance.id" ]
+    security_groups = [ "${aws_security_group.instance.id}" ]
 
     tags = {
         Name = "cert33-lb"
@@ -75,28 +75,28 @@ resource "aws_alb" "instance" {
 ### Add instance to target group
  
 resource "aws_lb_target_group_attachment" "instance" {
-    target_group_arn = "aws_alb_target_group.instance.arn"
-    target_id = "aws_instance.cert33.id"
+    target_group_arn = "${aws_alb_target_group.instance.arn}"
+    target_id = "${aws_instance.cert33.id}"
     port = 443
 }
  
 ### Add listener
  
 resource "aws_alb_listener" "instance_443" {
-    load_balancer_arn = "aws_alb.instance.arn"
+    load_balancer_arn = "${aws_alb.instance.arn}"
     port = "443"
     protocol = "HTTPS"
     ssl_policy = "ELBSecurityPolicy-2016-08"
-    certificate_arn = "var.aws_certificate_arn"
+    certificate_arn = "${var.aws_certificate_arn}"
 
     default_action {
         type = "forward"
-        target_group_arn = "aws_alb_target_group.instance.arn"
+        target_group_arn = "${aws_alb_target_group.instance.arn}"
     }
 }
  
 resource "aws_alb_listener" "instance_80" {
-    load_balancer_arn      = "aws_alb.instance.arn"
+    load_balancer_arn      = "${aws_alb.instance.arn}"
     port                                         = "80"
     protocol                                  = "HTTP"
 
@@ -118,13 +118,13 @@ data "aws_route53_zone" "selected" {
 }
  
 resource "aws_route53_record" "cert33" {
-            zone_id = "data.aws_route53_zone.selected.zone_id"
+            zone_id = "${data.aws_route53_zone.selected.zone_id}"
             name = "cert33"
             type = "A"       
 
     alias {
-        name = "aws_alb.instance.dns_name"
-        zone_id = "aws_alb.instance.zone_id"
+        name = "${aws_alb.instance.dns_name}"
+        zone_id = "${aws_alb.instance.zone_id}"
         evaluate_target_health = false
     }
 }
