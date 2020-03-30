434aa0bfc37941a83efbab5f5604e55755d5823a
Eric Wright <eric.wright@turbonomic.com>
2020-03-30 11:18:29 -0400

    adding outputs

--- artifacts/terraform/outputs.tf
+++ artifacts/terraform/outputs.tf
@@ -1,4 +1,4 @@
 output "public_ip" {
   description = "List of public IP addresses assigned to the instances"
-  value       = ["${aws_instance.web.*.public_ip}"]
+  value       = ["${aws_instance.instance.*.public_ip}"]
 }
