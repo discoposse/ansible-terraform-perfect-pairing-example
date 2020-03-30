70d2542e7afa05c80701ae6f0069b9d8642b6c41
Eric Wright <eric.wright@turbonomic.com>
2020-03-30 11:52:32 -0400

    fixing syntax

--- artifacts/terraform/simple-ec2.tf
+++ artifacts/terraform/simple-ec2.tf
@@ -59,7 +59,7 @@ resource "aws_instance" "cert-instance" {
       type        = "ssh"
       user        = "centos"
       private_key = var.aws_ssh_key
-      host        = "${aws_instance.cert-instance.*.public_ip}"
+      host        = var.aws_instance_name
     }
   }
 }
